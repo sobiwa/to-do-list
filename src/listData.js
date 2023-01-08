1d7c01f4165bd1a699f181efa4677931fd45ac2e
Sobiwa <kevin.b.collister@gmail.com>
2023-01-08 12:57:19 -0500

    fix

--- src/listData.js
+++ src/listData.js
@@ -3,6 +3,7 @@ import { add, startOfToday, parseISO } from "date-fns";
 let allProjects = {};
 
 function retrieveStorage() {
+    console.log('fix');
     let retrievedItems = [];
     for (let data in localStorage) {
         console.log(data);
